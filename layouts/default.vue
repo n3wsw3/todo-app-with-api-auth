ef6d61d9d817aa63fe008244cad43453ff65e272
Simon Uggla <simon.uggla@aol.com>
2021-10-12 18:06:24 +0200

    Initial

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main-wrapper">
+    <h1>This is a demo Application</h1>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.main-wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>{{ counter }}</p>
+  <button @click="increase">Increase Count!</button>
+</template>
+
+<script lang="ts" setup>
+let counter = ref(0);
+const increase = () => counter.value++;
+</script>
+
+<style></style>
