3069234339781cc47ba71c3a4bc1b19d0739bf37
Simon Uggla <simon.uggla@gmail.com>
2021-10-26 08:40:16 +0200

    Changed background color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="main-wrapper">
+    <nav>
+      Navbar template
+    </nav>
     <h1>This is a demo Application</h1>
     <slot></slot>
   </div>
 </template>
 
-<style lang="less" scoped>
+<style lang="less">
+@import "assets/definitions.less";
+
+body {
+  margin: 0;
+  padding: 0;
+
+  background-color: .colors()[BG];
+}
+
 .main-wrapper {
   max-width: 1200px;
   margin: 0 auto;
+
+  nav {
+    height: 50px;
+  }
+
+  h1 {
+    color: .colors()[text];
+  }
 }
 </style>
