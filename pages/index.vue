af1897c5f2485b08531fa57a9859388a7922bf53
Simon Uggla <simon.uggla@gmail.com>
2021-10-26 08:40:51 +0200

    Added showcase vue js code

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <p>{{ counter }}</p>
+  <p class="fuck">{{ data }}</p>
+  <p v-if="counter > 10">{{ counter }}</p>
   <button @click="increase">Increase Count!</button>
 </template>
 
 <script lang="ts" setup>
+const { data } = await useFetch("/api/hello");
 let counter = ref(0);
 const increase = () => counter.value++;
 </script>
-
-<style></style>
