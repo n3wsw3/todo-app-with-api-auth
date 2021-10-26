6e858a8ad93d585b1b5566f1744618c58840439a
Simon Uggla <simon.uggla@gmail.com>
2021-10-26 08:39:59 +0200

    Added component for showing circle

---
+++ components/UI/Circle.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="circle">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.circle {
+    border-radius: 100%;
+    width: 50px;
+    height: 50px;
+    border: 1px solid black;
+    display: inline-block;
+    
+    line-height: 100%;
+    text-align: center;
+    p {
+        display: inline-block;
+        line-height: normal;
+    }
+}
+
+</style>
