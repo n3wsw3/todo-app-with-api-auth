4dd8b2c192b6a523b0bf772239a26a0fdb5e45cd
Simon Uggla <simon.uggla@aol.com>
2021-11-06 00:33:27 +0100

    Added authentication middleware

---
+++ server/middleware/authenticate.ts
@@ -0,0 +1,8 @@
+import { ServerResponse as SR } from "http";
+import { IM } from "../../server";
+import { userFromToken } from "../services/token.service";
+
+export default async (req: IM, res: SR) => {
+  const token = req.headers.authorization;
+  req.user = await userFromToken(token);
+};
