38b71c5eb662ac7040fb51d756c8691db1fe3eb6
Simon Uggla <simon.uggla@aol.com>
2021-11-05 18:27:24 +0100

    Added test for api fetching

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
+  <p>{{ HelloAPI }}</p>
   <p>{{ counter }}</p>
   <button @click="increase">Increase Count!</button>
 </template>
 
 <script lang="ts" setup>
+const HelloAPI = await $fetch("/api/hello");
+
 let counter = ref(0);
 const increase = () => counter.value++;
 </script>
