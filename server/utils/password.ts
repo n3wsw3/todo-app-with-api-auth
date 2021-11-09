faf730124b754b3e92c1a16c3b2daf75d168aea9
Simon Uggla <simon.uggla@aol.com>
2021-11-09 20:46:49 +0100

    Replace crypto with argon2 for hashing passwords

--- server/utils/password.ts
+++ server/utils/password.ts
@@ -3,36 +3,27 @@
  * These are the only functions to be used when generating
  * and verifying password hashes.
  */
-import { createHmac, scryptSync, randomBytes, timingSafeEqual } from "crypto";
+import argon from "argon2";
+const { hash, argon2id, verify } = argon;
 
-export const generatePepperedPassword = (password: string): string => {
-  return createHmac("sha256", process.env.pepper ?? "Test")
-    .update(password)
-    .digest("base64");
+const OPTIONS = {
+  saltLength: 16,
+  hashLength: 32,
+  secret: Buffer.from(process.env.pepper ?? "Test"),
+  parallelism: 1,
+  timeCost: 3, // Iteration count
+  memoryCost: 1 << 11,
+  type: argon2id,
 };
 
-export const generatePasswordHash = (
-  password: string,
-  salt: string = randomBytes(16).toString("hex")
-): string => {
-  const hashedPassword = scryptSync(
-    generatePepperedPassword(password),
-    salt,
-    64
-  ).toString("base64");
-
-  return `${salt}:${hashedPassword}`;
+export const generatePasswordHash = (password: string) => {
+  return argonHash(password);
 };
 
-export const isPasswordMatch = (hash: string, password: string): boolean => {
-  const [salt, key] = hash.split(":");
-  const [_, testKey] = generatePasswordHash(password, salt).split(":");
+export const isPasswordMatch = async (argonhash: string, password: string) => {
+  return await verify(argonhash, password, OPTIONS);
+};
 
-  const keyBuffer = Buffer.from(key, "base64");
-  const testKeyBuffer = Buffer.from(testKey, "base64");
-
-  return (
-    keyBuffer.length === testKeyBuffer.length &&
-    timingSafeEqual(keyBuffer, testKeyBuffer)
-  );
+const argonHash = (password: string) => {
+  return hash(password, OPTIONS);
 };
