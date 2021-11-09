bb26040d5ef97f4dea9b5b02718e4245425289f6
Simon Uggla <simon.uggla@aol.com>
2021-11-09 19:43:01 +0100

    Creating express endpoint entry

---
+++ server/api/v1.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import mongoose from "mongoose";
+import UserError from "../utils/UserError";
+import routes from "../routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/", routes);
+
+app.use(
+  (err: Error | string, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof mongoose.Error.ValidationError) {
+      res.status(StatusCodes.BAD_REQUEST).json(err.errors);
+    } else if (err instanceof mongoose.Error) {
+      res.status(StatusCodes.BAD_REQUEST).json(err.message);
+    } else if (err instanceof UserError) {
+      res.status(err.code).send(err.message);
+    } else {
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send("Internal Server Error");
+    }
+  }
+);
+
+export default app;
