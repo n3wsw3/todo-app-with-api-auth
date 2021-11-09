b855a275f5b2425e3ca9e87e80e8116b575c1343
Simon Uggla <simon.uggla@aol.com>
2021-11-09 19:45:38 +0100

    Removed old authentication middleware

--- server/middleware/authenticate.ts
+++
@@ -1,9 +0,0 @@
-import { ServerResponse as SR } from "http";
-import { IM } from "../../server";
-import { userFromToken } from "../services/token.service";
-
-export default async (req: IM, res: SR) => {
-  const token = req.headers.authorization;
-  req.user = await userFromToken(token);
-  req.isAuthenticated = () => !!req.user;
-};
