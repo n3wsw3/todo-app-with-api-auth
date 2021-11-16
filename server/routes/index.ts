687763bf41e9e319262e25a812a58e5dc7a4933b
Simon Uggla <simon.uggla@gmail.com>
2021-11-16 08:40:12 +0100

    Added todo route to api router

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import hello from "./hello";
 import auth from "./auth";
+import todo from "./todo";
 
 const router = Router();
 
 router.use("/hello", hello);
 router.use("/auth", auth);
+router.use("/todo", todo);
 
 export default router;
