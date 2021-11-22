a86aded66d368519d65db20ac5967a8abf146fbe
Simon Uggla <simon.uggla@aol.com>
2021-11-22 23:51:49 +0100

    Use new Todo typings

--- server/routes/todo.ts
+++ server/routes/todo.ts
@@ -2,14 +2,19 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json([{
-    id: "12345",
-    title: "FÖRSTA TODO",
-    user: "192319823"
-}, {
-    id: "23434",
-    title: "ANDRA TODO",
-    user: "192319823"
-}]))
+router.get("/", (req, res) =>
+  res.json([
+    {
+      id: "12345",
+      message: "FÖRSTA TODO",
+      user: "192319823",
+    },
+    {
+      id: "23434",
+      message: "ANDRA TODO",
+      user: "192319823",
+    },
+  ] as Todo[])
+);
 
 export default router;
