a1c5f039dc1543eaa347ce6c4e1b573b7b0abd15
Simon Uggla <simon.uggla@aol.com>
2021-11-28 22:12:35 +0100

    Convert authentication middleware to be a function

--- server/utils/isAuthenticated.ts
+++ server/utils/isAuthenticated.ts
@@ -1,16 +1,12 @@
-import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import UserError from "./UserError";
 import { userFromToken } from "../services/token.service";
-import { catchAsync } from "./catchAsync";
+import { IncomingMessage } from "http";
 
-export default catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
-    req.user = await userFromToken(token);
-    if (!req.user)
-      next(new UserError("Invalid Access-Token", StatusCodes.BAD_REQUEST));
-
-    next();
-  }
-);
+export const useAuthenticatedUser = async (req: IncomingMessage) => {
+  const token = req.headers.authorization;
+  const user = await userFromToken(token);
+  if (!user)
+    throw new UserError("Invalid Access-Token", StatusCodes.BAD_REQUEST);
+  return user;
+};
