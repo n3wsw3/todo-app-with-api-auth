5aa608557bb45430e0a5ec67d5ebae8051b6d077
Simon Uggla <simon.uggla@aol.com>
2021-11-29 21:51:13 +0100

    Changed to test route params

--- server/routes/hello.ts
+++ server/routes/hello.ts
@@ -3,10 +3,15 @@ import { send, useBody, MIMES } from "h3";
 
 const router = createRouter();
 
-router.get("/", [(_, res) => "HELLO API!"]);
+router.get("/", [() => "HELLO API!"]);
 router.post("/", [
   async (req, res) => {
     return await send(res, await useBody(req), MIMES.json);
+  },
+]);
+router.put<{ id: string }>("/:id/hej", [
+  async (_, res, params) => {
+    return await send(res, params.id);
   },
 ]);
 
