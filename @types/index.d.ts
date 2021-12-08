7d8c4e2f30e4dad13accbd5f307af35989a02a05
Simon Uggla <simon.uggla@aol.com>
2021-12-08 23:09:51 +0100

    Added global types for user and token

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,13 +1,20 @@
 import { ITodo } from "../server/models/todo.model";
+import { IToken } from "../server/models/token.model";
+import { IUser } from "../server/models/user.model";
 
 type Model = { id: string };
 
 type UITodo = Omit<ITodo, "user"> & { user: string };
+type UIToken = Omit<IToken, "user">;
+type UIUser = Omit<IUser, "password">;
 
 declare global {
   type DeleteRequest = Model;
 
   type Todo = UITodo & Model;
-  type TodoUpdateRequest = Partial<UITodo>;
+  type TodoUpdateRequest = Partial<Omit<UITodo, "user">>;
   type TodoCreateRequest = UITodo;
+
+  type User = UIUser & Model;
+  type Token = UIToken;
 }
