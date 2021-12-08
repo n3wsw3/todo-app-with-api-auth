69b1adc567b4be7a1e739a2437f72ff708a4ec98
Simon Uggla <simon.uggla@aol.com>
2021-12-08 23:12:28 +0100

    Added logging for cookies and auith header

--- server/middleware/logger.ts
+++ server/middleware/logger.ts
@@ -1,5 +1,11 @@
 import { IncomingMessage as IM, ServerResponse as SR } from "http";
+import { useCookies } from "h3";
 
 export default async (req: IM, res: SR) => {
-  console.log(req.url, req.method);
+  console.log(
+    req.url,
+    req.method,
+    await useCookies(req),
+    req.headers.authorization ?? ""
+  );
 };
