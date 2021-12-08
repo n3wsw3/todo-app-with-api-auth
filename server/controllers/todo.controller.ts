cb227e1da173d164a0744de51860167733cbd7cc
Simon Uggla <simon.uggla@aol.com>
2021-12-08 23:10:40 +0100

    Added controllers for creating, updating, fetching

--- server/controllers/todo.controller.ts
+++ server/controllers/todo.controller.ts
@@ -2,10 +2,48 @@ import { StatusCodes } from "http-status-codes";
 import { Todo } from "../models/todo.model";
 import { useBody } from "h3";
 import { IncomingMessage, ServerResponse } from "http";
+import { useAuthenticatedUser } from "../utils/isAuthenticated";
+
+const Handler = <K = {}>(
+  handle: <T extends K>(
+    req: IncomingMessage,
+    res: ServerResponse,
+    params?: T
+  ) => any
+) => handle;
 
 export const createTodo = async (req: IncomingMessage, res: ServerResponse) => {
-  return await new Todo(await useBody(req)).save().then(async (todo) => {
-    res.statusCode = StatusCodes.CREATED;
-    return todo;
-  });
+  const todo: TodoCreateRequest = await useBody(req);
+  const user = await useAuthenticatedUser(req);
+
+  return await new Todo({ ...todo, user: user.id })
+    .save()
+    .then(async (todo) => {
+      res.statusCode = StatusCodes.CREATED;
+      return todo;
+    });
 };
+
+export const getTodos = Handler(async (req, res) => {
+  const user = await useAuthenticatedUser(req);
+
+  return await Todo.find({ user: user.id });
+});
+
+export const getTodo = Handler<{ id: string }>(async (req, res, params) => {
+  const user = await useAuthenticatedUser(req);
+
+  return await Todo.findOne({ id: params.id, user: user.id });
+});
+
+export const updateTodo = Handler<{ id: string }>(async (req, res, params) => {
+  const user = await useAuthenticatedUser(req);
+  const body = await useBody(req);
+
+  delete body.user;
+  delete body.id;
+
+  return await Todo.findOneAndUpdate({ id: params.id, user: user.id }, body, {
+    returnDocument: "after",
+  });
+});
