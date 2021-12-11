8db965d918c64f4b61e8b02f9ddb1d6fc1f78fb8
Simon Uggla <simon.uggla@aol.com>
2021-12-12 00:13:52 +0100

    Added new styling to todos

--- components/UI/todo.vue
+++ components/UI/todo.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="todo">
-    <div v-if="!isEditing" class="message">{{ todo.message }}</div>
+    <div v-if="!isEditing" class="begin">
+      <label class="input" :for="todo.id + 'done'">
+        <input
+          type="checkbox"
+          name="isDone"
+          :id="todo.id + 'done'"
+          v-model="isDone"
+        />
+        <span></span>
+      </label>
+      <p class="message">{{ todo.message }}</p>
+    </div>
     <div v-else>
       <input type="text" v-model="tempValue" />
-      <button @click="save">Save</button>
     </div>
     <div class="buttons">
-      <button @click="edit">Edit</button>
-      <button class="close">X</button>
+      <button v-if="!isEditing" @click="edit" class="primary">Edit</button>
+      <button v-else @click="save" class="primary">Save</button>
+      <button>Delete</button>
     </div>
   </div>
 </template>
@@ -16,12 +27,13 @@
 const props = defineProps<{ todo: Todo }>();
 const emit = defineEmits<{ (e: "update", msg: string): void }>();
 
-let isEditing = ref(false);
+const isEditing = ref(false);
+const isDone = ref(false);
 
-let tempValue = ref(props.todo.message);
+const tempValue = ref(props.todo.message);
 
 const edit = () => {
-  isEditing.value = true;
+  isEditing.value = !isEditing.value;
 };
 
 const save = () => {
@@ -31,37 +43,137 @@ const save = () => {
 </script>
 <style lang="less" scoped>
 @import "../assets/definitions.less";
+@size: 30px;
 .todo {
   display: flex;
   justify-content: space-between;
   background-color: .colors() [BG-plus];
-  padding: 0.5em;
-  border-radius: 6px;
+  padding: 0.7em;
+  border-radius: 10px;
   margin: 10px 0;
-}
-.message {
-  line-height: 1em;
+  box-shadow: 2px 3px 4px 0px rgb(0 0 0 / 25%), 2px 2px 9px 2px rgb(0 0 0 / 22%);
+  > * {
+    height: @size;
+  }
+  .begin {
+    display: flex;
+    @csize: 20px;
+    label {
+      display: block;
+      position: relative;
+      width: @csize;
+      height: @csize;
+      cursor: pointer;
+      font-size: 22px;
+      user-select: none;
+      margin: auto;
+      margin-right: 10px;
+
+      span {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: @csize;
+        width: @csize;
+
+        border: 2px solid .colors() [text];
+        border-radius: 100%;
+
+        &:hover {
+          border-color: .colors() [text-hover];
+        }
+
+        &:active {
+          border-color: .colors() [text-click];
+        }
+      }
+
+      input {
+        position: absolute;
+        opacity: 0;
+        cursor: pointer;
+        height: 0;
+        width: 0;
+        &:checked ~ span {
+          border: none;
+          &:after {
+            content: "🗸";
+            height: 100%;
+            font-size: @csize;
+            display: block;
+            line-height: @csize - 2;
+            padding-left: 3px;
+          }
+        }
+      }
+    }
+
+    .message {
+      margin: 0;
+      line-height: @size;
+    }
+  }
 }
 .buttons {
   display: flex;
   button {
     margin: 0 0.2em;
     border: none;
-    border-radius: 2px;
+    border-radius: 10px;
     cursor: pointer;
     height: auto;
 
-    &.close {
-      background-color: .colors() [error];
-      box-shadow: 1px 1px 0px 0px .colors() [error-click];
+    padding: 5px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-width: 2px;
+    line-height: @size - 2 * $padding - 2 * $border-width;
+    font-size: @size - 2 * $padding - 2 * $border-width;
+
+    color: .colors() [primary];
+
+    background: linear-gradient(.colors() [BG-plus], .colors() [BG-plus])
+        padding-box,
+      linear-gradient(to right, .colors() [primary], .colors() [primary-hover])
+        border-box;
+    border: 2px solid transparent;
+
+    &:hover {
+      background: linear-gradient(.colors() [BG-plus], .colors() [BG-plus])
+          padding-box,
+        linear-gradient(
+            to right,
+            .colors() [primary-hover],
+            .colors() [primary-click]
+          )
+          border-box;
+    }
+
+    &:active {
+      background: linear-gradient(.colors() [BG-plus], .colors() [BG-plus])
+          padding-box,
+        .colors() [primary-click] border-box;
+    }
+
+    &.primary {
+      border: none;
+      background: linear-gradient(
+        to right,
+        .colors() [primary],
+        .colors() [primary-hover]
+      );
+      color: .colors() [text];
 
       &:hover {
-        background-color: .colors() [error-hover];
+        background: linear-gradient(
+          to right,
+          .colors() [primary-hover],
+          .colors() [primary-click]
+        );
       }
 
       &:active {
-        box-shadow: none;
-        background-color: .colors() [error-click];
+        box-shadow: inset 1px 1px 3px 0px .colors() [primary-click];
       }
     }
   }
