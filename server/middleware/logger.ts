e4916d1fca1cadce17c88a2f7c22c2497ac4b281
Simon Uggla <simon.uggla@aol.com>
2021-12-13 19:05:02 +0100

    Stop logging auth-token

--- server/middleware/logger.ts
+++ server/middleware/logger.ts
@@ -1,11 +1,5 @@
 import { IncomingMessage as IM, ServerResponse as SR } from "http";
-import { useCookies } from "h3";
 
 export default async (req: IM, res: SR) => {
-  console.log(
-    req.url,
-    req.method,
-    await useCookies(req),
-    req.headers.authorization ?? ""
-  );
+  console.log(req.url, req.method);
 };
